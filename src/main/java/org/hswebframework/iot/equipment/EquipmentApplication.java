4957095931f7c9c8c655e26dfcd527290478c4a7
Administrator <1278824628@qq.com>
2019-01-08 16:31:37 +0800

    修改配置文件

---
+++ src/main/java/org/hswebframework/iot/equipment/EquipmentApplication.java
@@ -0,0 +1,13 @@
+package org.hswebframework.iot.equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class EquipmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentApplication.class, args);
+    }
+
+}
+
